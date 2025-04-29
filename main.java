7523a956891d3c76eacba7b4db2ab2ec3b63b59d
Mohit Verma <mohit51341862@gmail.com>
2025-04-29 12:26:10 +0530

    main

--- main.java
+++ main.java
@@ -1,17 +1,11 @@
-
 import java.util.*;
 public class main {
+    public static int largestElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1];
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr =  new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int result = 0;
-        for (int i = 0; i < n; i=i+2) {
-            result += arr[i];
-        }
-        System.out.println(result);
+        int[] arr = {3,3,6,1};
+        System.out.println(largestElement(arr));
     }
 }
