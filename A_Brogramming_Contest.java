15dcc1ad804f27b27f7b373b5c5b5b7f52ede883
Mohit Verma <mohit51341862@gmail.com>
2025-08-29 20:36:24 +0530

    A

---
+++ A_Brogramming_Contest.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Brogramming_Contest {
+    static PrintWriter out = new PrintWriter(System.out);
+    static FastReader in = new FastReader();
+
+    public static void main(String[] args) throws Exception {
+        int t = in.nextInt();
+        while (t-- > 0) solve();
+        out.flush();
+    }
+
+    static void solve() {
+        int n =in.nextInt();
+        String s = in.next();
+        int res = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s.charAt(i) != s.charAt(i+1)) res++;
+        }
+        if(s.charAt(0) =='1') res++;
+        System.out.println(res);
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
