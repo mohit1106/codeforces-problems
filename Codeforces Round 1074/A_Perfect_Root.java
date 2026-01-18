fe17cc11c4251cb7f5f7d49f1d5a3ca99a3435c3
Mohit Verma <mohit51341862@gmail.com>
2026-01-18 21:17:27 +0530

    round 1074

---
+++ Codeforces Round 1074/A_Perfect_Root.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Perfect_Root {
+    static PrintWriter out = new PrintWriter(System.out);
+    static FastReader in = new FastReader();
+
+    public static void main(String[] args) throws Exception {
+        int t = in.nextInt();
+        while (t-- > 0) solve();
+        out.flush();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
---
+++ Codeforces Round 1074/B_Prefix_Max.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class B_Prefix_Max {
+    static PrintWriter out = new PrintWriter(System.out);
+    static FastReader in = new FastReader();
+
+    public static void main(String[] args) throws Exception {
+        int t = in.nextInt();
+        while (t-- > 0) solve();
+        out.flush();
+    }
+
+    static void solve() {
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+            max = Math.max(max, arr[i]);
+        }
+        System.out.println(max*n);
+    }
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
